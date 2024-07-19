2c82ef5ba32a7b8f7b696443dd72886eb3a872b9
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2024-07-18 21:36:23 -0300

    so fazendo um teste rapido com prompt pra mudar o nome

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { Name } from './components/Name';
+import React, { useState, useEffect } from "react";
+import { Name } from "./components/Name";
 
 function App() {
-  const [nome, setNome] = useState('');
+  const [nome, setNome] = useState("");
   const [idade, setIdade] = useState();
 
   // Use useEffect para definir os valores uma vez
   useEffect(() => {
-    setNome('Perboles');
+    setNome("Perboles");
     setIdade(18);
   }, []);
 
+  function handleChangeName() {
+    const name = prompt("Insira seu nome");
+    setIdade(name);
+  }
   return (
     <div>
       <div>oi Migo tudo bom?</div>
       <Name aluno={nome} idade={idade} />
+      <button onClick={handleChangeName}>Mudar nome</button>
     </div>
   );
 }
